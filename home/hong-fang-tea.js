03b9404f1ab3faac916958850be452de6ce3b07f
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-14 17:29:37 -0600

    Create hong-fang-tea.js

---
+++ home/hong-fang-tea.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	while(true) {
+		await ns.hack('hong-fang-tea');
+    await ns.grow('hong-fang-tea');
+    await ns.weaken('hong-fang-tea');
+	}
+}
