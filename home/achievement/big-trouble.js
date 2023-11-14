baf286cc50c150f953c0a31a69a9a5b919138056
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-14 17:17:01 -0600

    Update big-trouble.js

--- home/achievement/big-trouble.js
+++ home/achievement/big-trouble.js
@@ -1,5 +1,6 @@
 /** @param {NS} ns */
 /* Used for the "Big trouble" achievement */
+/* Run after installing augmentations */
 export async function main(ns) {
 	while(true) {
 		await ns.hack('n00dles');
