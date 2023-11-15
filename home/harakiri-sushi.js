8978be78614b9c1fa57633c36b8bd865c7581af3
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-14 19:02:19 -0600

    Create harakiri-sushi.js

---
+++ home/harakiri-sushi.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	while(true) {
+		await ns.hack('harakiri-sushi');
+    await ns.grow('harakiri-sushi');
+    await ns.weaken('harakiri-sushi');
+	}
+}
