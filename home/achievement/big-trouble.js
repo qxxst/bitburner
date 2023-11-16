f0ad1696251c2d6dbab2033c8e7db619fd814dad
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-15 23:32:59 -0600

    Update big-trouble.js

--- home/achievement/big-trouble.js
+++ home/achievement/big-trouble.js
@@ -2,8 +2,12 @@
 /* Used for the "Big trouble" achievement */
 /* Run after installing augmentations */
 export async function main(ns) {
-	while(true) {
+	while (true) {
 		await ns.hack('n00dles');
-    await ns.weaken('n00dles');
+		// Checks if the security level is greater than the minimum before running weaken
+		const securityLevel = await ns.getServerSecurityLevel('n00dles')
+		if (securityLevel > 1) {
+			await ns.weaken('n00dles');
+		}
 	}
 }
