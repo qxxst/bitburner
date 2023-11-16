5e37196782960fc06be2bd3a77a1371ec2fca8c0
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-15 21:20:03 -0600

    Create silver-helix.js

---
+++ home/silver-helix.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	while(true) {
+		await ns.hack('silver-helix');
+    await ns.grow('silver-helix');
+    await ns.weaken('silver-helix');
+	}
+}
