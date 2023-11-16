c0b0ca6ebae0a2d194d13c05f42a6bfeaecf5317
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-16 00:00:30 -0600

    Update scripts

--- home/achievement/big-trouble.js
+++ home/achievement/big-trouble.js
@@ -2,11 +2,12 @@
 /* Used for the "Big trouble" achievement */
 /* Run after installing augmentations */
 export async function main(ns) {
+	const securityLevel = await ns.getServerSecurityLevel('n00dles')
+	const minSecurityLevel = await ns.getServerSecurityLevel('n00dles');
 	while (true) {
 		await ns.hack('n00dles');
 		// Checks if the security level is greater than the minimum before running weaken
-		const securityLevel = await ns.getServerSecurityLevel('n00dles')
-		if (securityLevel > 1) {
+		if (securityLevel > minSecurityLevel) {
 			await ns.weaken('n00dles');
 		}
 	}
--- home/drain.js
+++ home/drain.js
@@ -1,9 +1,14 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	const target = ns.args[0];
+	const securityLevel = await ns.getServerSecurityLevel(target);
+	const minSecurityLevel = await ns.getServerSecurityLevel(target);
 	while (true) {
 		await ns.hack(target);
     	await ns.grow(target);
-    	await ns.weaken(target);
+		// Checks if the security level is greater than the minimum before running weaken
+		if (securityLevel > minSecurityLevel) {
+			await ns.weaken(target);
+		}
 	}
 }
