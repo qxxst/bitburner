b76a7194bcad185532dd49e9faf480c4adb9f07b
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-20 23:49:56 -0600

    Update scripts

--- home/drain.js
+++ home/drain.js
@@ -2,13 +2,16 @@
 export async function main(ns) {
 	const target = ns.args[0];
 	const minSecurityLevel = await ns.getServerMinSecurityLevel(target);
+	const moneyThreshold = await ns.getServerMaxMoney(target);
 	while (true) {
 		// Checks if the security level is greater than the minimum before running weaken
 		var securityLevel = await ns.getServerSecurityLevel(target);
 		if (securityLevel > minSecurityLevel) {
 			await ns.weaken(target);
 		}
+		if (moneyThreshold > ns.getServerMoneyAvailable(target)) {
+    		await ns.grow(target);
+		}
 		await ns.hack(target);
-    	await ns.grow(target);
 	}
 }
--- home/entry.js
+++ home/entry.js
@@ -7,5 +7,14 @@ export async function main(ns) {
     if (ns.fileExists("FTPCrack.exe", "home")) {
         ns.ftpcrack(target);
     }
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+        ns.relaysmtp(target);
+    }
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+        ns.httpworm(target);
+    }
+    if (ns.fileExists("SQLInject.exe", "home")) {
+        ns.sqlinject(target);
+    }
     ns.nuke(target);
 }
