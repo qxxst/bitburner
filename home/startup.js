b9719c8c9ddb561d1fa2b9b6ef6d3a67490f61ee
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-21 00:14:56 -0600

    Update startup.js

--- home/startup.js
+++ home/startup.js
@@ -1,20 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    // Array of all servers that don't need any ports opened to gain root access. These have 16 GB of RAM
     const servers0Port = ["sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea", "harakiri-sushi"];
-
-    // Array of all servers that only need 1 port opened to gain root access. These have 32 GB of RAM
     const servers1Port = ["neo-net", "zer0", "max-hardware", "iron-gym", "CSEC"];
+    const servers2Port = ["phantasy", "omega-net", "silver-helix", "johnson-ortho", "the-hub", "crush-fitness", "avmnite-02h"];
+    const servers3Port = ["computek", "netlink", "rothman-uni", "catalyst", "summit-uni", "I.I.I.I"];
+    const servers4Port = ["syscore"];
+    const servers5Port = ["zb-institute", "darkweb"];
 
-    const servers2Port = ["phantasy", "omega-net", "silver-helix"];
-
-    // Copy our scripts onto each server that requires 0 ports to gain root access. Then use nuke() to gain admin access and run the scripts.
     for (let i = 0; i < servers0Port.length; ++i) {
         const serv = servers0Port[i];
         ns.exec("nuke.js", serv);
     }
 
-    // Wait until we acquire the "BruteSSH.exe" program
     while (!ns.fileExists("BruteSSH.exe")) {
         await ns.sleep(60000);
     }
