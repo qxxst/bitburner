9cd519abcccdbd7c0d5a8738ddf874e2b11eb02d
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-29 21:35:16 -0600

    Update best.js

--- home/best.js
+++ home/best.js
@@ -1,4 +1,19 @@
 /** @param {NS} ns */
 export async function main(ns) {
-
+    var possiblePorts = 0
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+        var possiblePorts = possiblePorts + 1;
+    }
+    if (ns.fileExists("FTPCrack.exe", "home")) {
+        var possiblePorts = possiblePorts + 1;
+    }
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+        var possiblePorts = possiblePorts + 1;
+    }
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+        var possiblePorts = possiblePorts + 1;
+    }
+    if (ns.fileExists("SQLInject.exe", "home")) {
+        var possiblePorts = possiblePorts + 1;
+    }
 }
