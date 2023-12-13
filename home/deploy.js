89c0753f9999cf7dce64e46d975f1b0e9ca1a08a
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-12 20:43:23 -0600

    Update deploy.js

--- home/deploy.js
+++ home/deploy.js
@@ -13,6 +13,25 @@ export async function main(ns) {
 		return;
 	}
 
+    const target = host
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+        ns.brutessh(target);
+    }
+    if (ns.fileExists("FTPCrack.exe", "home")) {
+        ns.ftpcrack(target);
+    }
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+        ns.relaysmtp(target);
+    }
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+        ns.httpworm(target);
+    }
+    if (ns.fileExists("SQLInject.exe", "home")) {
+        ns.sqlinject(target);
+    }
+    
+    ns.nuke(target);
+
 	const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
 	await ns.scp(script, host, home);
 	ns.exec(script, host, threads);
