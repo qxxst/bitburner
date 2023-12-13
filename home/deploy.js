12cadeb3263cdafbee8c6620deb43351543dd4cb
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-12 20:42:25 -0600

    Update files

--- home/deploy.js
+++ home/deploy.js
@@ -1,17 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	const args = ns.flags([["help", false]]);
-	if (args.help || args._.length < 2) {
-		ns.tprint("This script deploys another script on a server with maximum threads possible.");
-		ns.tprint(`Usage: run ${ns.getScriptName()} HOST ARGUMENTS`);
-		ns.tprint("Example:");
-		ns.tprint(`> run ${ns.getScriptName()} n00dles foodnstuff`);
-		return;
-	}
-
-	const host = args._[0];
-	const script = "drain.js"
-	const script_args = args._.slice(1);
+	const host = ns.args[0];
+	const script = "scp/drain.js"
+    const home = "home"
 
 	if (!ns.serverExists(host)) {
 		ns.tprint(`Server '${host}' does not exist. Aborting.`);
@@ -23,7 +14,6 @@ export async function main(ns) {
 	}
 
 	const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
-	ns.tprint(`Launching script '${script}' on server '${host}' with ${threads} threads and the following arguments: ${script_args}`);
-	await ns.scp(script, host);
+	await ns.scp(script, host, home);
 	ns.exec(script, host, threads);
 }
