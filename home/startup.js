f0b31b02f8e6c7f4a5ee3b2b9b3d53a27fc5d72d
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-16 03:19:03 -0600

    Update startup.js

--- home/startup.js
+++ home/startup.js
@@ -18,7 +18,7 @@ export async function main(ns) {
     const servers4Port = ["syscore", "alpha-ent", "global-pharm", "lexo-corp", "snap-fitness", "univ-energy", "nova-med", "unitalife", "zb-def", "aevum-police", "."];
     const servers5Port = ["zb-institute", "galactic-cyber", "deltaone", "taiyang-digital", "aerocorp", "omnia", "icarus", "infocomm", "defcomm", "solaris", "zeus-med", "helios", "omnitek", "powerhouse-fitness"];
 
-    const doNotTarget = ["darkweb", "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "w0r1d_d43m0n", "fulcrumassets", "nwo", "clarkinc"];
+    const doNotTarget = ["darkweb", "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "w0r1d_d43m0n", "fulcrumassets", "nwo", "clarkinc", "johnson-ortho"];
 
     function hasDuplicates(array) {
         return new Set(array).size !== array.length;
