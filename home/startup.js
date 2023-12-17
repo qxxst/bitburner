350ce8dc7ed7744a5950a0ae529cd6fb95c7035f
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-17 01:53:32 -0600

    Update startup.js

--- home/startup.js
+++ home/startup.js
@@ -17,7 +17,7 @@ export async function main(ns) {
     const servers2Port = ["phantasy", "omega-net", "silver-helix", "the-hub", "crush-fitness"];
     const servers3Port = ["computek", "netlink", "rothman-uni", "catalyst", "summit-uni", "rho-construction", "millenium-fitness"];
     const servers4Port = ["syscore", "alpha-ent", "global-pharm", "lexo-corp", "snap-fitness", "univ-energy", "nova-med", "unitalife", "zb-def", "aevum-police", "."];
-    const servers5Port = ["zb-institute", "galactic-cyber", "deltaone", "taiyang-digital", "aerocorp", "omnia", "icarus", "infocomm", "defcomm", "solaris", "zeus-med", "helios", "omnitek", "powerhouse-fitness", "vitalife", "titan-labs", "microdyne", "blade"];
+    const servers5Port = ["zb-institute", "galactic-cyber", "deltaone", "taiyang-digital", "aerocorp", "omnia", "icarus", "infocomm", "defcomm", "solaris", "zeus-med", "helios", "omnitek", "powerhouse-fitness", "vitalife", "titan-labs", "microdyne", "blade", "fulcrumtech"];
 
     // Do not target servers with no usable RAM, or story-related servers
     const doNotTarget = ["darkweb", "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "w0r1d_d43m0n", "fulcrumassets", "nwo", "clarkinc", "johnson-ortho", "applied-energetics", "storm-tech", "4sigma"];
