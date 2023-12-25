2b54d18fa53773f8426b6185bb423f8505ab852c
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-25 02:11:48 -0600

    Update dev-menu.js

--- home/achievement/dev-menu.js
+++ home/achievement/dev-menu.js
@@ -1,7 +1,7 @@
-// achievement/dev-menu.js by yichizhng
+// achievement/dev-menu.js by yichizhng. Modified by qxxst
 /** @param {NS} ns */
 // Used for the "Exploit: You're not meant to access this." achievement
 export async function main(ns) {
-    globalThis.webpack_require ?? webpackChunkbitburner.push([[-1], {}, w => globalThis.webpack_require = w]);
-    Object.keys(webpack_require.m).forEach(k => Object.values(webpack_require(k)).forEach(p => p?.toPage?.('Dev')));
-  }
+  globalThis.webpack_require ?? webpackChunkbitburner.push([[-1], {}, w => globalThis.webpack_require = w]);
+  Object.keys(webpack_require.m).forEach(k => Object.values(webpack_require(k)).forEach(p => p?.toPage?.('Dev')));
+}
