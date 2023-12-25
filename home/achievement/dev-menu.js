601a3fcc75997abb35efac3bef37be0e70d90859
qxxst <31394614+qxxst@users.noreply.github.com>
2023-12-25 00:44:23 -0600

    Create dev-menu.js

---
+++ home/achievement/dev-menu.js
@@ -0,0 +1,7 @@
+// dev-menu.js by yichizhng
+/** @param {NS} ns */
+// Used for the "Exploit: You're not meant to access this." achievement
+export async function main(ns) {
+    globalThis.webpack_require ?? webpackChunkbitburner.push([[-1], {}, w => globalThis.webpack_require = w]);
+    Object.keys(webpack_require.m).forEach(k => Object.values(webpack_require(k)).forEach(p => p?.toPage?.('Dev')));
+  }
